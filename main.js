70beba4ce21996c12124714ea0d8de50951f6fa2
Vidit-Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2022-01-01 18:28:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function here()
+{
+    window.alert("Bhanu Kapahi: she behaved like she is my mother my first best favourite teacher ever")
+}
+
+function here1()
+{
+window.alert    ("Archna mam: my second best favourite teacher ever")
+}
+
